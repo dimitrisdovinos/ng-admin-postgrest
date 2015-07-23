553b87e8162a833f385f09155a5b950a6a4d27b9
Dimitris <dimitris.dovinos@gmail.com>
2015-07-23 11:02:19 +0300

    trying to get it to work with js_usr

--- main.js
+++ main.js
@@ -17,7 +17,7 @@
                 delete params._perPage;
 
                 if (params._sortField) {
-                    params.order = params._sortField + '.' + params._sortDir.toLowerCase();
+                    params.order = 'usr_id' + '.' + params._sortDir.toLowerCase();
                     delete params._sortField;
                     delete params._sortDir;
                 }
@@ -44,7 +44,7 @@
 
                     if (pattern.test(config.url)) {
                         config.params = config.params || {};
-                        config.params['id'] = 'eq.' + pattern.exec(config.url)[1];
+                        config.params['usr_id'] = 'eq.' + pattern.exec(config.url)[1];
                         config.url = config.url.replace(pattern, '');
                     }
 
@@ -59,171 +59,55 @@
 
         var app = nga
             .application('Ng-admin + PostgREST')
-            .baseApiUrl('https://postgrest.herokuapp.com/');
+            .baseApiUrl('http://localhost:3000/');
 
-        var speaker = nga.entity('speakers');
-        var session = nga.entity('sessions');
-        var sponsor = nga.entity('sponsors');
+        var js_usr = nga.entity('js_usr').identifier(nga.field('usr_id'));
+        // var session = nga.entity('sessions');
+        // var sponsor = nga.entity('sponsors');
+
 
         app
-            .addEntity(speaker)
-            .addEntity(session)
-            .addEntity(sponsor);
+            .addEntity(js_usr);
 
-        // speaker views -------------------------------------------------------
+        // js_usr views -------------------------------------------------------
 
-        speaker.menuView()
+        js_usr.menuView()
             .icon('<span class="glyphicon glyphicon-user"></span>');
 
-        speaker.dashboardView()
-            .title('Last speakers')
+        js_usr.dashboardView()
+            .title('Last js_usrs')
             .fields([
-                nga.field('id'),
-                nga.field('name'),
-                nga.field('twitter'),
-                nga.field('featured').type('boolean'),
-                nga.field('lineup_order').type('number'),
+                nga.field('usr_id'),
+                nga.field('email_nm'),
+                nga.field('crt_dt'),
             ]);
 
-        speaker.listView()
+        js_usr.listView()
             .perPage(10)
             .fields([
-                nga.field('id'),
-                nga.field('name'),
-                nga.field('twitter'),
-                nga.field('featured').type('boolean'),
-                nga.field('lineup_order').type('number'),
+                nga.field('usr_id'),
+                nga.field('email_nm'),
+                nga.field('crt_dt'),
             ])
             .listActions(['edit', 'show']);
 
-        speaker.showView()
+        js_usr.showView()
             .fields([
-                nga.field('id'),
-                nga.field('name'),
-                nga.field('twitter'),
-                nga.field('avatar_url'),
-                nga.field('bio').type('text'),
-                nga.field('featured').type('boolean'),
-                nga.field('lineup_order').type('number'),
+                nga.field('usr_id'),
+                nga.field('email_nm'),
+                nga.field('crt_dt'),
             ]);
 
-        speaker.creationView()
+        js_usr.creationView()
             .fields([
-                nga.field('name'),
-                nga.field('twitter'),
-                nga.field('avatar_url'),
-                nga.field('bio').type('text'),
-                nga.field('featured').type('boolean'),
-                nga.field('lineup_order').type('number'),
+                nga.field('usr_id'),
+                nga.field('email_nm'),
+                nga.field('crt_dt'),
             ]);
 
-        speaker.editionView()
-            .fields(speaker.creationView().fields());
+        js_usr.editionView()
+            .fields(js_usr.creationView().fields());
 
-        // session views -------------------------------------------------------
-
-        session.menuView()
-            .icon('<span class="glyphicon glyphicon-calendar"></span>');
-
-        session.dashboardView()
-            .title('Last sessions')
-            .fields([
-                nga.field('id'),
-                nga.field('speaker_id', 'reference')
-                    .label('Speaker')
-                    .targetEntity(speaker)
-                    .targetField(nga.field('name')),
-                nga.field('start_time'),
-                nga.field('end_time'),
-                nga.field('location'),
-                nga.field('session_type'),
-            ]);
-
-        session.listView()
-            .fields([
-                nga.field('id'),
-                nga.field('speaker_id', 'reference')
-                    .label('Speaker')
-                    .targetEntity(speaker)
-                    .targetField(nga.field('name')),
-                nga.field('start_time'),
-                nga.field('end_time'),
-                nga.field('location'),
-                nga.field('session_type'),
-            ])
-            .filters([
-                nga.field('speaker_id', 'reference')
-                    .label('Speaker')
-                    .targetEntity(speaker)
-                    .targetField(nga.field('name')),
-                nga.field('location'),
-            ])
-            .listActions(['edit', 'show']);
-
-        session.showView()
-            .fields([
-                nga.field('id'),
-                nga.field('speaker_id'),
-                nga.field('start_time'),
-                nga.field('end_time'),
-                nga.field('location'),
-                nga.field('session_type'),
-            ]);
-
-        session.creationView()
-            .fields([
-                nga.field('speaker_id', 'reference')
-                    .targetEntity(speaker)
-                    .targetField(nga.field('name')),
-                nga.field('start_time'),
-                nga.field('end_time'),
-                nga.field('location'),
-                nga.field('session_type'),
-            ]);
-
-        session.editionView()
-            .fields(session.creationView().fields());
-
-        // sponsor views -------------------------------------------------------
-
-        sponsor.menuView()
-            .icon('<span class="glyphicon glyphicon-gift"></span>');
-
-        sponsor.dashboardView()
-            .title('Last sponsors')
-            .fields([
-                nga.field('id'),
-                nga.field('name'),
-                nga.field('site_url'),
-            ]);
-
-        sponsor.listView()
-            .fields([
-                nga.field('id'),
-                nga.field('name'),
-            ])
-            .listActions(['edit', 'show']);
-
-        sponsor.showView()
-            .fields([
-                nga.field('id'),
-                nga.field('name'),
-                nga.field('site_url'),
-                nga.field('logo_url'),
-            ]);
-
-        sponsor.creationView()
-            .fields([
-                nga.field('name'),
-                nga.field('site_url'),
-                nga.field('logo_url'),
-            ]);
-
-        sponsor.editionView()
-            .fields(sponsor.creationView().fields());
-
-        // ---------------------------------------------------------------------
-
-        nga.configure(app);
+          nga.configure(app);
     });
 }());
